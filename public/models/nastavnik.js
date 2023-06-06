880413757de45fafb13dcbf7088309a272d6ebf0
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-06-06 23:47:20 +0200

    Initial commit

--- public/models/nastavnik.js
+++ public/models/nastavnik.js
@@ -4,5 +4,6 @@ const sequelize = require("../db");
 const Nastavnik = sequelize.define("nastavnik", {
     username: Sequelize.STRING,
     password_hash: Sequelize.STRING
+
 });
 module.exports = Nastavnik;
